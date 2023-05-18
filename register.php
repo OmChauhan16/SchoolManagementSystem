1d0d47dbdfe0cbe6a23754c4e600a95a4f1a3f3e
omchauhan2 <omc0784@gmail.com>
2023-05-18 17:50:10 +0530

    om

--- register.php
+++ register.php
@@ -59,31 +59,27 @@
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fa fa-sitemap"></i></span>
 							</div>
-
                   <input type="text" id="organization" name='organization' class="form-control form-control-sm oname"  placeholder="Organization Name*"  />
-                  
                 </div>
-                <div>
-                  <p id="orgcheck"></p>
-                  </div>
-
+                <p id="orgcheck"></p>
+                  
                 <div class="form-outline mb-4">
                 <div class="input-group mb-2">
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fas fa-lock"></i></span>
 							</div>
                 <input type="text" id="adminname" name="adminname" class="form-control form-control-sm" placeholder="Admin Name*" />
-                 
               </div>
               <p id="adcheck"></p> 
+
                 <div class="form-outline mb-4">
                 <div class="input-group mb-3">
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fas fa-envelope"></i></span>
 							</div>
                 <input type="email" id="email" name="email" class="form-control form-control-sm" placeholder="Email*" />
-                <p id="emailcheck"></p>  
-              </div>
+                </div>
+              <p id="emailcheck"></p>
 
                 <div class="form-outline mb-4">
                 <div class="input-group mb-3">
@@ -91,8 +87,8 @@
 								<span class="input-group-text"><i class="fas fa-phone"></i></span>
 							</div>              
                 <input type="number" id="phone" name="phone" class="form-control form-control-sm" placeholder="Phone*" />
-                <p id="phonecheck"></p>  
               </div>
+              <p id="phonecheck"></p>
 
                 <div class="form-outline mb-4">
                 <div class="input-group mb-3">
@@ -100,8 +96,8 @@
 								<span class="input-group-text"><i class="fas fa-key"></i></span>
 							</div>                 
                <input type="password" id="password" name="password" class="form-control form-control-sm" placeholder="Password*" />
-                  <p id="passcheck"></p>
                 </div>
+                <p id="passcheck"></p>
 
                 <div class="form-outline mb-4">
                 <div class="input-group mb-3">
@@ -109,9 +105,8 @@
 								<span class="input-group-text"><i class="fas fa-check-circle"></i></span>
 							</div>                 
                <input type="password" id="conpassw" name="conpassw" class="form-control form-control-sm" placeholder="Confirm Password*" />
-                  <p id="cpasscheck"></p>
                 </div>
-
+                <p id="cpasscheck"></p>
                 
                 <div class="d-flex justify-content-center">
                   <button type="button" id="register" name="register"
@@ -140,8 +135,19 @@
 $(document).ready(function(){
 
 
-  $('#orgcheck').hide();
-   var org_err =true;
+   $('#orgcheck').hide();
+   $('#adcheck').hide();
+   $('#emailcheck').hide();
+   $('#phonecheck').hide();
+   $('#passcheck').hide();
+   $('#cpasscheck').hide();
+
+   var org_err = true;
+   var ad_err = true;
+   var e_err = true;
+   var ph_err = true;
+   var pass_err = true;
+   var cpass_err = true;
    
   $('#organization').keyup(function(){
    org_validate();
@@ -153,7 +159,7 @@ $(document).ready(function(){
     $('#orgcheck').html('**Please enter organization name');
     $('#orgcheck').focus();
     $('#orgcheck').css("color","red");
-    var org_err = false;
+     org_err = false;
     return false;
    }
    else{
@@ -164,7 +170,7 @@ $(document).ready(function(){
      $('#orgcheck').html('**Length must be betweeen 5 and 30');
      $('#orgcheck').focus();
      $('#orgcheck').css("color","red");
-     var org_err = false;
+     org_err = false;
     return false
     }
     else{
@@ -172,42 +178,268 @@ $(document).ready(function(){
     }
 
     if(orgname.match(/^[a-zA-Z]*$/)){
-    $('#orgcheck').hide
+    $('#orgcheck').hide();
    }
    else{
     $('#orgcheck').show();
     $('#orgcheck').html('** name must contain albhabets only');
     $('#orgcheck').focus();
     $('#orgcheck').css("color","red");
-    var org_err = false;
+     org_err = false;
     return false;
    }
   }
+
+  // ADMIN NAME VALIDATIONS.......................................................
+
+  $('#adminname').keyup(function(){
+     adminname_check();
+
+  });
+
+  function adminname_check(){
+   var ad_name = $('#adminname').val();
+   
+   if(ad_name.length == ""){
+    $('#adcheck').show();
+    $('#adcheck').html('**Please enter admin name');
+    $('#adcheck').focus();
+    $('#adcheck').css("color","red");
+    ad_err = false;
+    return false;
+   }
+   else{
+    $('#adcheck').hide();
+
+   }
+
+   if(ad_name.length<5 || ad_name.length>30){
+    $('#adcheck').show();
+    $('#adcheck').html('**Length must be betweeen 5 and 30');
+    $('#adcheck').focus();
+    $('#adcheck').css("color","red");
+    ad_err = false;
+    return false;
+   }
+   else{
+    $('#adcheck').hide();
+   }
+
+   if(ad_name.match(/^[a-zA-Z]*$/)){
+    $('#adcheck').hide();
+   }
+   else{
     
+    $('#adcheck').show();
+    $('#adcheck').html('**name must contain albhabets only');
+    $('#adcheck').focus();
+    $('#adcheck').css("color","red");
+    ad_err = false;
+    return false;
+   }
+  }
+
+  // EMAIL VALIDATIONS.......................................................
+
+
+  $('#email').keyup(function(){
+     email_check();
+
+  });
+
+  function email_check(){
+   var email = $('#email').val();
+   
+   if(email.length == ""){
+    $('#emailcheck').show();
+    $('#emailcheck').html('**Please enter email name');
+    $('#emailcheck').focus();
+    $('#emailcheck').css("color","red");
+    e_err = false;
+    return false;
+   }
+   else{
+    $('#emailcheck').hide();
+   }
+
+   if(email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+    $('#emailcheck').hide();
+   }
+   else{
+    
+    $('#emailcheck').show();
+    $('#emailcheck').html('**please enter valid email');
+    $('#emailcheck').focus();
+    $('#emailcheck').css("color","red");
+    e_err = false;
+    return false;
+   }
+  }
+
+  // PHONE VALIDATIONS...................
+
+  $("#phone").keyup(function(){
+    phone_check();
+  });
+
+  function phone_check(){
+  var phone = $("#phone").val();
+
+  if(phone.length == ""){
+    $('#phonecheck').show();
+    $('#phonecheck').html('**Please enter phone no.');
+    $('#phonecheck').focus();
+    $('#phonecheck').css("color","red");
+    ph_err = false;
+    return false;
+   }
+   else{
+    $('#phonecheck').hide();
+   }
+   if(phone.length == 10 ){
+    $('#phonecheck').hide();
+   }
+   else{
+    $('#phonecheck').show();
+    $('#phonecheck').html('**Invalid Length ');
+    $('#phonecheck').focus();
+    $('#phonecheck').css("color","red");
+    ph_err = false;
+    return false;
+   }
+
+   if(phone[0] == 6 || phone[0]== 7 || phone[0]==8 || phone[0]==9){
+    $('#phonecheck').hide();
+   }
+   else{
+    $('#phonecheck').show();
+    $('#phonecheck').html('**Invalid phone no. ');
+    $('#phonecheck').focus();
+    $('#phonecheck').css("color","red");
+    ph_err = false;
+    return false;
+   }
+}
+
+
+// PASSWORD VALIDATIONS..............................
+
+$("#password").keyup(function(){
+  pass_validate();
+});
+
+  function pass_validate(){
+  var password = $("#password").val();
+  if(password.length == ""){
+    $('#passcheck').show();
+    $('#passcheck').html('**Please enter password');
+    $('#passcheck').focus();
+    $('#passcheck').css("color","red");
+    pass_err = false;
+    return false;
+   }
+   else{
+    $('#passcheck').hide();
+   }
+   if(password.length != 8){
+    $('#passcheck').show();
+    $('#passcheck').html('**Password must have 8 characters');
+    $('#passcheck').focus();
+    $('#passcheck').css("color","red");
+    pass_err = false;
+    return false;
+   }
+   else{
+    $('#passcheck').hide();
+   }
+
+   if(password.match((/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))){
+    $('#passcheck').hide();
+   }
+
+   else{
+    $('#passcheck').show();
+    $('#passcheck').html('**please enter a strong password');
+    $('#passcheck').focus();
+    $('#passcheck').css("color","red");
+    pass_err = false;
+    return false;
+   }
+  }
+
+  $("#conpassw").keyup(function(){
+   confirm_password();
+  });
+
+ function confirm_password(){
+  var password = $("#password").val();
+  var cpass = $("#conpassw").val();
+  if(cpass == password){
+    $('#cpasscheck').hide();
+  }
+  else{
+    $('#cpasscheck').show();
+    $('#cpasscheck').html('**not matched with password');
+    $('#cpasscheck').focus();
+    $('#cpasscheck').css("color","red");
+    cpass_err  = false;
+    return false;
+  }
+  }
   $("#register").click(function(){
 
-  var organization = $('#organization').val();
-  var register = $("#register").val();
+    var organization = $('#organization').val();
+    var admin = $("#adminname").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var pass = $("#password").val();
+    var cpass = $("#conpassw").val();
+    var register = $("#register").val(); 
 
-  org_err = true;
+    org_err = true;
+    ad_err = true;
+    e_err = true;
+    ph_err = true;
+    pass_err = true;
+    cpass_err = true;
 
   org_validate();
+  adminname_check();
+  email_check();
+  phone_check();
+  pass_validate();
+  confirm_password();
 
-  
-   
+  if(org_err == true &&  ad_err == true && e_err==true && ph_err == true 
+  && pass_err == true && cpass_err == true )
+   {
+    
+    
     $.ajax({
      url:"registerbackend.php",
      type:"POST",
      data:{organization:organization,
+           admin:admin,
+           email:email,
+           phone:phone,
+           pass:pass,
+           cpass:cpass,
            register:register
      },    
      success:function(data){
-       console.log('gud');
+       console.log(data);
      }
+    })
+   }
+
+   else
+   {
+    return false;
+   }
    
   });
 });
-});
+
 
 
   
--- registerbackend.php
+++ registerbackend.php
@@ -1,21 +1,31 @@
 <?php
 include("database.php");
 
-// echo '<pre>';
-// print_r($_POST);
-// exit;
 
-//  if(isset($_POST['register'])){
- if(!empty($_POST['organization']) ){
+ if(isset($_POST['register'])){
+ if(!empty($_POST['organization']) && !empty($_POST['admin']) && !empty($_POST['email'])
+ && !empty($_POST['phone']) && !empty($_POST['pass']) )
+ {
 
  
       $oname= $_POST['organization'];
-       
+      $admin = $_POST['admin'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $pass = $_POST['pass'];
         
-        $sql= "INSERT INTO `register` (`orgname`) VALUE ('$oname')";
-        $q = mysqli_query($conn,$sql);
+$sql1 = "INSERT INTO `organization` (`contactName`,`name`,`phoneNumber`
+,`email`,`password`,`createdDate`,`modifiedDate`) VALUES ('$admin','$oname','$phone','$email',
+'$pass',NOW(),NOW())";
+         $q1 = mysqli_query($conn,$sql1);
+
+
+$sql2 = "INSERT INTO `loginmaster` (`mobileNo`,`email`,`password`,`organization_Id`,`createDate`,`modifiedDate`)
+VALUES ('$phone','$email','$pass','',NOW(),now()) ";
+   $q2 = mysqli_query($conn,$sql2);
+
      }
-//  }
+  }
 //  if($query){
 //     echo "REGISTERED";
 //  }
